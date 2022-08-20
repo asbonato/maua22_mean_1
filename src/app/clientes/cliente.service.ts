e8d6b8bc8dba26e18871ac80c3372b498472843b
asbonato <asbonato@gmail.com>
2022-08-20 15:24:06 -0300

    roteamento do backend e navegação - apostila 7 até listagem 2.3.3

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -3,13 +3,14 @@ import { Cliente } from "./cliente.model";
 import { Subject } from "rxjs";
 import { HttpClient } from "@angular/common/http";
 import { map } from 'rxjs/operators'
+import { Router } from "@angular/router";
 
 @Injectable({ providedIn: 'root' })
 export class ClienteService {
   private clientes: Cliente[] = [];
   private listaClientesAtualizada = new Subject<Cliente[]>();
 
-  constructor(private httpClient: HttpClient){}
+  constructor(private httpClient: HttpClient, private router:Router){}
 
   getCliente(idCliente:string){
     //return {...this.clientes.find((cli) => cli.id === idCliente)};
@@ -51,6 +52,7 @@ export class ClienteService {
         cliente.id = dados.id;
         this.clientes.push(cliente);
         this.listaClientesAtualizada.next([...this.clientes]);
+        this.router.navigate(['/'])
       }
     )
   }
@@ -79,6 +81,7 @@ export class ClienteService {
       copia[indice] = cliente;
       this.clientes = copia;
       this.listaClientesAtualizada.next([...this.clientes]);
+      this.router.navigate(['/'])
     });
   }
 }
